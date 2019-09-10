3cde56654247643832967c993ce9c888f6a5885e
Kara Hartig <kara_hartig@g.harvard.edu>
2019-09-10 17:09:08 -0400

    Set up minimal structure
    Used python-packaging.readthedocs.io as a reference to set up minimal
    structure: setup.py file, __init__.py (currently empty) for package,
    MANIFEST.in to include non-Python files in setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def readme():
+	with open('README.md') as f:
+		return f.read()
+
+setup(name='camtrack',
+	version='0.1',
+	description='Workflow for running HYSPLIT trajectories on cold air outbreaks from Community Atmosphere Model, CAM4',
+	long_description=readme(),
+	url='https://github.com/kahartig/camtrack.git',
+	author='Kara Hartig',
+	author_email='kara_hartig@g.harvard.edu',
+	license='GNUv3',
+	packages=['camtrack'],
+#	install_requires=[],
+	include_package_data=True,
+	zip_safe=False,
+	test_suite='nose.collector',
+	tests_require=['nose'])
