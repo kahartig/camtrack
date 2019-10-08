d9bda3dabf54301ca9bc8e2940fdf56789e57d9f
Kara Hartig <kara_hartig@g.harvard.edu>
2019-10-08 14:50:55 -0400

    Write docstrings for identify_cold_events

--- camtrack/identify_cold_events.py
+++ camtrack/identify_cold_events.py
@@ -19,13 +19,47 @@ import os
 
 def subset_by_timelatlon(filename, winter_idx, desired_variable_key, lat_lower_bound, lon_bounds, landfrac_min=0.9, testing=False):
 	"""
-	filename: path to concatenated netCDF file
-	winter_idx: index of winter under study
-	desired_variable_key: string; key of variable to be subset (must be 3-D: [time, lat, lon])
-	lat_lower_bound: integer or float giving lower bound of latitude; upper bound will be maximum lat value (N pole steroegraphic)
-	lon_bounds: list-like of [min_lon, max_lon]
-	landfrac_min: minimum value of landfraction categorizing a grid point as "on land" 
-	if testing=True, uses a different bound for max_time to accomodate sample CAM4 file for nosetests and includes unmasked data in output
+	Take a subset in time and lat/lon of the variable specified by
+	desired_variable_key from filename and mask by landfraction. The time subset
+	runs from December 1st through February 28th of the following year and the
+	spatial extent must include the north pole, requiring only a lower bound for
+	latitude.
+
+	Parameters
+	----------
+	filename: string
+		path to netCDF file of CAM4 output covering at least Dec through Feb of a given year
+	winter_idx: integer
+		index of winter under study. 0 is 07-08 year, 1 is 08-09, etc.
+	desired_variable_key: string
+		key of variable to be subset by time, latitude, and longitude
+		the corresponding data must have the dimensions data[time, lat, lon]
+	lat_lower_bound: integer or float
+		lower bound of latitude range for the subset; upper bound is North Pole
+		latitude subset = [lat_lower_bound, 90.0]
+	lon_bounds: array-like of floats
+		lower and upper bounds of longitude range for the subset
+		must be in the order (lower bound, upper bound)
+		longitude subset = [lon_bounds[0], lon_bounds[1]]
+	landfrac_min: float between 0 and 1
+		minimum value of landfraction for which a gridpoint will be considered "on land"
+	testing: boolean
+		if testing=True, activates special conditions on time bounds and output for nosetests
+		default is False
+
+	Returns
+	-------
+	subset_dict: dictionary
+		'data': a subset from time=Dec 1st - Feb 28th,
+			latitude=[lat_lower_bound, 90.0],
+			longitude=[lon_bounds[0], lon_bounds[1]] of the variable
+			desired_variable_key, masked by landfraction so that any points with
+			landfraction < landfrac_min have a value of np.nan
+		'time': array of ordinal time values corresponding to subsetted time dimension
+		'lat': array of latitudes corresponding to subsetted lat dimension
+		'lon': array of longitudes corresponding to subsetted lon dimension
+		if testing=True, also includes:
+			'unmasked_data': same as 'data' but without the landfraction masking
 	"""
 	nc_file = Dataset(filename)
 	variable_object = nc_file.variables[desired_variable_key]
@@ -62,9 +96,34 @@ def subset_by_timelatlon(filename, winter_idx, desired_variable_key, lat_lower_b
 
 def slice_from_bounds(file, dimension_key, low_bound, upper_bound=np.inf):
 	"""
-	file: Dataset instance
-	creates a slice for the interval [low_bound, upper_bound]
-	raises an error if any non-infinite bounds are outside the range of the dimension
+	Given closed bounds [low_bound, upper_bound], return a slice object of the
+	given dimension that spans the range low_bound <= dimension <= upper_bound.
+
+	For example, if dim is the array of values in the dimension, then dim[slice]
+	will return those values of dim in the closed interval
+	[low_bound, upper_bound]. If var is a variable with the corresponding
+	dimension, var[dimension], then var[slice] will return the values of var at
+	locations where dim is in the closed interval [low_bound, upper_bound].
+
+	Raises an error if any of the non-infinite bounds are outside the range of
+	the dimension.
+
+	Parameters
+	----------
+	file: instance of netCDF4 Dataset
+		netCDF file containing the dimension to be sliced
+	dimension_key: string
+		name of dimension to be sliced
+	low_bound: float or -np.inf
+		lower bound of closed dimension slice
+		if -np.inf, lower bound will be the lowest value in the dimension
+	upper_bound: float or np.inf
+		upper bound of closed dimension slice
+		if np.inf, upper bound will be the highest value in the dimension
+
+	Returns
+	-------
+	slice object spanning the closed interval [low_bound, upper_bound] of the dimension
 	"""
 	if not isinstance(file, Dataset):
 		raise TypeError('File argument must be an instance of the netCDF4 Dataset class; given type {}'.format(type(file)))
@@ -92,28 +151,69 @@ def slice_from_bounds(file, dimension_key, low_bound, upper_bound=np.inf):
 
 def find_overall_cold_events(data_dict, winter_idx, number_of_events, distinct_conditions):
 	"""
-	two events are "indistinct" only if they violate all three distinct_conditions
-	data_dict: dictionary with 'data', 'time', 'lat', and 'lon' keys
-		data must have dimensions data[time, lat, lon]
-	distinct_conditions = dictionary with conditions that determine whether events are distinct
-		includes: 'min time separation' (fractional days), 'min lat separation' (degrees), 'min lon separation' (degrees)
+	Identify the x coldest distinct events for the temperature data given in
+	data_dict, where x=number_of_events and the conditions for distinctness are
+	given in distinct_conditions.
+
+	Two events, where 'events' are separate entries in the temperature data
+	contained in data_dict, are considered distinct if the absolute
+	difference in the times, latitudes, and longitudes of the two events are
+	greater than or equal to the minimum separations given in
+	distinct_conditions. Two events are indistinct only if all three
+	distinctness conditions are violated.
+
+	Parameters
+	----------
+	data_dict: dictionary
+		'data': array of temperature data with dimensions data[time, lat, lon]
+		'time': array of ordinal time values corresponding time dimension
+		'lat': array of latitudes corresponding to lat dimension
+		'lon': array of longitudes corresponding to lon dimension
+	winter_idx: integer
+		index of winter under study. 0 is 07-08 year, 1 is 08-09, etc.
+	number_of_events: integer
+		number of distinct cold events to identify, starting with the coldest
+	distinct_conditions: dictionary
+		'min time separation': fractional days; minimum separation between two
+			events in time to be considered time-distinct
+		'min lat separation': degrees; minimum separation between two events in
+			latitude to be considered latitude-distinct
+		'min lon separation': degrees; minimum separation between two events in
+			longitude to be considered longitude-distinct
+
+	Returns
+	-------
+	cold_events: Pandas DataFrame
+		index: integer; 0 for coldest event, 1 for second-coldest, etc.
+		columns:
+			'winter index': winter_idx
+			'time index': index of data_dict['time'] corresponding to event
+			'time': time of event in days since 0001-01-01 on 'noleap' calendar
+			'lat index': index of data_dict['lat'] corresponding to event
+			'lat': latitude of event in degrees on -90 to 90 scale
+			'lon index': index of data_dict['lon'] corresponding to event
+			'lon': longitude of event in degrees on 0 to 360 scale
+			'2m temp': temperature (in K) at 2 meters of the event 
 	"""
 	t2m_on_land = data_dict['data']
 	times = data_dict['time']
 	latitudes = data_dict['lat']
 	longitudes = data_dict['lon']
 
-	# sort 2m temperatures from lowest to highest
-	# sorted_idx is a tuple of index arrays; for ex, times[sorted_idx[0][0]] is the time value of the very coldest point in t2m_on_land, latitudes[sorted_idx[1][0]] is the corresponding latitude, etc.
+	# Sort 2m temperatures from lowest to highest
+	# sorted_idx is a tuple of index arrays
+		# first index is dimension: 0=time, 1=lat, 2=lon
+		# second index is sorted order: coldest value occurs at time
+		#     times[sorted_idx[0][0]], lat latitudes[sorted_idx[1][0]], etc.
 	sorted_idx = np.unravel_index(t2m_on_land.argsort(axis=None), t2m_on_land.shape)
 
-	# initialize DataFrame of cold events
+	# Initialize DataFrame of cold events
 	cold_events = pd.DataFrame(index=range(number_of_events), columns=['winter index', 'time index', 'time', 'lat index', 'lat', 'lon index', 'lon', '2m temp'])
 
-	# store first (coldest) event
+	# Store first (coldest) event
 	cold_events.loc[0] = [winter_idx, sorted_idx[0][0], times[sorted_idx[0][0]], sorted_idx[1][0], latitudes[sorted_idx[1][0]], sorted_idx[2][0], longitudes[sorted_idx[2][0]], t2m_on_land[sorted_idx[0][0], sorted_idx[1][0], sorted_idx[2][0]]]
 
-	# find distinct cold events, starting with second-coldest
+	# Find distinct cold events, starting with second-coldest
 	idx = 1
 	num_found = 1
 	while num_found < number_of_events:
@@ -150,35 +250,48 @@ def find_overall_cold_events(data_dict, winter_idx, number_of_events, distinct_c
 
 def print_CONTROL_files(events, traj_heights, backtrack_time, output_dir, traj_dir, data_file_path):
     """
-    Prints out the CONTROL files that HYSPLIT uses to set up a backtracking run using the entries in events.
-    Creates CONTROL files in output_dir. Also creates a subdirectory called traj_dir, which must be in the
-    HYSPLIT working directory, in which the trajectory file will be placed when HYSPLIT is run with these
-    CONTROL files.
+    Generate the CONTROL files that HYSPLIT uses to set up a backtracking run
+    based on the entries in 'events'.
+
+    Also creates two directories:
+    	output_dir: where CONTROL files are placed after creation
+    	traj_dir: where trajectory files will be placed when HYSPLIT is run with
+    		these CONTROL files
 
     CONTROL files will be named CONTROL_winter<winter idx>_event<events idx>
-    	winter idx: taken from 'winter idx' key in events; 0 corresponds to 07-08 year, 1 to 08-09, etc.
+    	winter idx: taken from 'winter idx' key in events
+    		0 corresponds to 07-08 year, 1 to 08-09, etc.
     	events idx: the DataFrame index of each event in the events DataFrame
     trajectory files will be named traj_winter<winter idx>_event<events idx>
 
-    Parameters:
-    events: Pandas DataFrame containing details of the particular cold events for which the CONTROL files
-    are being generated. events must contain at least the following:
-        winter idx = index of the winter during which the event takes place (07-08 is 0, 08-09 is 1 etc)
-        time = the time of the event as a real number of days since 0001-01-01 00:00:00 (fractional part for hours)
-        lat = latitude coordinate of the event as a real number of degrees on -90 to 90 scale
-        lon = longitude coordinate of the event as a real number of degrees on 0 to 360 scale
-    traj_heights: array-like of starting height(s) in meters for each event 
-    backtrack_time: hours back in time to follow trajectories; must be positive integer
-    output_dir: output directory for CONTROL files
-    traj_dir: HYSPLIT directory to output trajectory files
-    data_file_path: full path and filename of CAM4 data file
+    Parameters
+    ----------
+    events: Pandas DataFrame
+    	each index/row corresponds to a distinct cold event for which a CONTROL
+    	file will be generated. Columns must include:
+    		'winter index': index of winter during which the event takes place
+    			(07-08 is 0, 08-09 is 1 etc)
+    		'time': time of event in days since 0001-01-01 on 'noleap' calendar
+    		'lat': latitude of event in degrees on -90 to 90 scale
+    		'lon': longitude of event in degrees on 0 to 360 scale
+    traj_heights: array-like
+    	starting heights in meters for each trajectory
+    backtrack_time: integer
+    	number of hours to follow each trajectory back in time
+    output_dir: string
+    	output directory for CONTROL files
+    traj_dir: string
+    	HYSPLIT directory to output trajectory files
+    data_file_path: string
+    	full path and filename of CAM4 data file from which to calculate
+    	trajectories
     """
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     if not os.path.exists(traj_dir):
         os.makedirs(traj_dir)
     for ev_idx, ev_row in events.iterrows():
-    	control_path = os.path.join(output_dir, 'CONTROL_winter{:d}_event{:02d}'.format(ev_row['winter idx'], ev_idx))
+    	control_path = os.path.join(output_dir, 'CONTROL_winter{:d}_event{:02d}'.format(ev_row['winter index'], ev_idx))
     	with open(control_path, 'w') as f:
     		data_path, data_filename = os.path.split(data_file_path)
     		t = cftime.num2date(ev_row['time'], 'days since 0001-01-01 00:00:00', calendar='noleap')
@@ -211,4 +324,4 @@ def print_CONTROL_files(events, traj_heights, backtrack_time, output_dir, traj_d
     		# Output trajectory file path:
     		f.write(traj_dir + '\n')
     		# Output trajectory file name:
-    		f.write('traj_winter{}_event{}'.format(ev_row['winter idx'], ev_idx))
+    		f.write('traj_winter{}_event{}'.format(ev_row['winter index'], ev_idx))
