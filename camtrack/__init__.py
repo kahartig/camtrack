897a0d45831d30217ffb99d3c0270a8724e81bfe
Kara Hartig <kara_hartig@g.harvard.edu>
2020-01-21 18:46:42 -0500

    Update dependencies to include cartopy

--- camtrack/__init__.py
+++ camtrack/__init__.py
@@ -1,2 +1,3 @@
 from .identify_cold_events import subset_by_timelatlon, slice_from_bounds, find_overall_cold_events, print_CONTROL_files
 from .climatology import compare_to_climatology, sort_and_find_coldest
+from .plot_along_trajectory import DataFromTrajectoryFile
