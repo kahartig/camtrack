b710befe642222fc6551ee01bab06065139d7481
Kara Hartig <kara_hartig@g.harvard.edu>
2020-05-26 21:01:31 -0400

    Update __init__ to add data.subset_and_mask function

--- camtrack/__init__.py
+++ camtrack/__init__.py
@@ -1,3 +1,3 @@
 from .climatology import anomaly_DJF, sample_coldtail, find_coldest, OLD_find_coldest
-from .data import TrajectoryFile, WinterCAM, subset_nc, slice_dim, make_CONTROL
+from .data import TrajectoryFile, WinterCAM, subset_and_mask, subset_nc, slice_dim, make_CONTROL
 from .parceltrack import ClimateAlongTrajectory
