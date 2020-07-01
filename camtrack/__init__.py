76ec7d9c41a5eddfec8a9a7c7bdd98dcc58ed0ff
Kara Hartig <kara_hartig@g.harvard.edu>
2020-07-01 18:17:37 -0400

    Add new plot functions to __init__.py
    plot.trajectory_path_with_wind
    plot.trajectory_endpoints_plot

--- camtrack/__init__.py
+++ camtrack/__init__.py
@@ -1,4 +1,4 @@
 from .climatology import anomaly_DJF, sample_coldtail, find_coldest, OLD_find_coldest
 from .data import TrajectoryFile, WinterCAM, subset_and_mask, subset_nc, slice_dim, make_CONTROL
 from .parceltrack import ClimateAlongTrajectory
-from .plot import trajectory_path_plots, line_plots_by_event, line_plots_by_trajectory, contour_plots, generate_trajlist, generate_traj2save, generate_tnum2save
+from .plot import trajectory_path_plots, trajectory_path_with_wind, trajectory_endpoints_plot, line_plots_by_event, line_plots_by_trajectory, contour_plots, generate_trajlist, generate_traj2save, generate_tnum2save
