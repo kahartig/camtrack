dbb7158d6e72fd34356b5c8666270ab63b3ae460
Kara Hartig <kara_hartig@g.harvard.edu>
2020-08-18 16:41:32 -0600

    Re-write find_coldest()
    Adapt read-in of climatology_dict to accomodate new format for anomaly_DJF
      using xarray
    Add two helper functions
    	convert_times: given cftime.DatetimeNoLeap, return ordinal time and
    	  date string
    	samples2values: after sorting and unraveling temperature array,
    	  convert linear sample indices to coordinate indices and values
    In loop over sorted events, store linear indices of distinct cold events and
      generate cold events dataframe at the end instead of pre-allocating and adding
      events row-by-row
    
    MINIMALLY TESTED
    	Need to add nosetests

--- camtrack/climatology.py
+++ camtrack/climatology.py
@@ -170,48 +170,63 @@ def find_coldest(climatology_dict, number_of_events, distinct_conditions):
         'temp anomaly': temperature anomaly associated with the event; taken
             from climatology_dict['diff'] at the time, lat, and lon listed
     """
-    temperature_anomaly = climatology_dict['diff']
-    times = climatology_dict['time']
-    latitudes = climatology_dict['lat']
-    longitudes = climatology_dict['lon']
+    def convert_times(cf_datetime):
+        """cf_datetime is a list of cftime.DatetimeNoLeap instance(s) """
+        ordinal_time = cftime.date2num(cf_datetime, 'days since 0001-01-01 00:00:00', calendar='noleap')
+        datestring = [t.strftime() for t in cf_datetime]
+        return ordinal_time, datestring
+
+    def samples2values(samples_idx, sorted_coord_idx, coordinates):
+        # sorted_coord_idx = sorted_idx[index of desired coordinate][:]
+        coordinate_idx = [sorted_coord_idx[idx] for idx in samples_idx]
+        coordinate_values = [coordinates[c_idx] for c_idx in coordinate_idx]
+        return coordinate_values, coordinate_idx
+
+    temperature_anomaly = climatology_dict['diff'].values
+    times = climatology_dict['diff'].time.values
+    latitudes = climatology_dict['diff'].lat.values
+    longitudes = climatology_dict['diff'].lon.values
 
     # Sort by temperature anomaly, from coldest (most negative) to warmest (most positive and NaN)
     sorted_idx = np.unravel_index(temperature_anomaly.argsort(axis=None), temperature_anomaly.shape)
 
-    # Initialize cold_events DataFrame
-    cold_events = pd.DataFrame(index=range(number_of_events), columns=['date', 'time', 'lat', 'lon', 'temp anomaly'])
-    date_coldest = cftime.num2date(times[sorted_idx[0][0]], 'days since 0001-01-01 00:00:00', calendar='noleap')
-    cold_events.loc[0] = ['{:04d}-{:02d}-{:02d}'.format(date_coldest.year, date_coldest.month, date_coldest.day), times[sorted_idx[0][0]], latitudes[sorted_idx[1][0]], longitudes[sorted_idx[2][0]], temperature_anomaly[sorted_idx[0][0], sorted_idx[1][0], sorted_idx[2][0]]]
-
     # Find distinct cold events, starting with second-coldest
+    event_idx = [0] # store all distinct event indices, starting with coldest
     idx = 1
     num_found = 1
     print('Starting loop over sorted temperature anomalies to identify cold events...')
     while ((num_found < number_of_events) and (idx < len(times))):
-        time_idx = sorted_idx[0][idx]
-        lat_idx = sorted_idx[1][idx]
-        lon_idx = sorted_idx[2][idx]
-        time = times[time_idx]
-        date = cftime.num2date(time, 'days since 0001-01-01 00:00:00', calendar='noleap')
-        date_string = '{:04d}-{:02d}-{:02d}'.format(date.year, date.month, date.day)
-        lat = latitudes[lat_idx]
-        lon = longitudes[lon_idx]
+        time = times[sorted_idx[0][idx]]
+        lat = latitudes[sorted_idx[1][idx]]
+        lon = longitudes[sorted_idx[2][idx]]
+        [ord_time], [datestring] = convert_times([time])
         # check distinctness conditions:
         distinct = True
-        for found_idx, found_row in cold_events.iterrows():
-            # NOTE: iterates over empty, pre-allocated rows as well, but should not affect distinct flag
-            if ((abs(time - found_row['time']) < distinct_conditions['min time separation'])
-                and (abs(lat - found_row['lat']) < distinct_conditions['min lat separation'])
-                and (abs(lon - found_row['lon']) < distinct_conditions['min lon separation'])):
+        found_time, _ = samples2values(event_idx, sorted_idx[0], times)
+        found_lat, _ = samples2values(event_idx, sorted_idx[1], latitudes)
+        found_lon, _ = samples2values(event_idx, sorted_idx[2], longitudes)
+        found_ord, found_datestring = convert_times(found_time)
+        for f_time, f_lat, f_lon in zip(found_ord, found_lat, found_lon):
+            if ((abs(ord_time - f_time) < distinct_conditions['delta t'])
+                and (abs(lat - f_lat) < distinct_conditions['delta lat'])
+                and (abs(lon - f_lon) < distinct_conditions['delta lon'])):
                 # indistinct only if overlapping another event simultaneously in time and location
                 distinct = False
         if distinct:
-            cold_events.loc[num_found]['date'] = date_string
-            cold_events.loc[num_found]['time'] = time
-            cold_events.loc[num_found]['lat'] = lat
-            cold_events.loc[num_found]['lon'] = lon
-            cold_events.loc[num_found]['temp anomaly'] = temperature_anomaly[time_idx, lat_idx, lon_idx]
+            event_idx.append(idx)
             num_found = num_found + 1
         idx = idx + 1
-    print('Found {:d} distinct cold events out of the {:d} coldest datapoints in sorted temperature anomaly array'.format(number_of_events, idx-1))
+    print('Found {:d} distinct cold events out of the {:d} coldest datapoints in sorted temperature anomaly array'.format(num_found, idx-1))
+    
+    # Pull coordinate values cooresponding to samples
+    sample_times, sample_times_idx = samples2values(event_idx, sorted_idx[0], times)
+    sample_lat, sample_lat_idx = samples2values(event_idx, sorted_idx[1], latitudes)
+    sample_lon, sample_lon_idx = samples2values(event_idx, sorted_idx[2], longitudes)
+    sample_temp_anomaly = [temperature_anomaly[t, la, lo] for t,la,lo in zip(sample_times_idx, sample_lat_idx, sample_lon_idx)]
+
+    # Convert sampled times to ordinal time and datestring
+    ordinal_times, datestrings = convert_times(sample_times)
+
+    # Generate cold events dataframe
+    cold_events = pd.DataFrame.from_dict({'time': ordinal_times, 'date': datestrings, 'cftime date': sample_times, 'lat': sample_lat, 'lon': sample_lon, 'temp anomaly': sample_temp_anomaly})
     return cold_events
