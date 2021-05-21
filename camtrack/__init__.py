94da49b502863f4756a01b54d6c9cafb4b925dd7
Kara Hartig <kara_hartig@g.harvard.edu>
2021-05-21 12:02:41 -0400

    New assist function: set matplotlib default font sizes
    assist.set_fontsize()
    	Takes 4 fontsizes to set: titles (axes + figure), axis labels, legend,
    	  and other (ticks and all other text)

--- camtrack/__init__.py
+++ camtrack/__init__.py
@@ -2,5 +2,5 @@ from .climatology import anomaly_DJF, sample_coldtail, find_coldest
 from .data import TrajectoryFile, WinterCAM, subset_and_mask, make_CONTROL
 from .parceltrack import ClimateAlongTrajectory
 from .plot import anomaly_histogram, trajectory_path_plots, trajectory_path_with_wind, trajectory_endpoints_plot, line_plots_by_event, line_plots_by_trajectory, contour_plots, generate_trajlist, generate_traj2save, generate_tnum2save
-from .assist import roll_longitude
+from .assist import roll_longitude, set_fontsize
 from .cluster import shift_origin, cluster_paths, cluster_line_plots
--- camtrack/assist.py
+++ camtrack/assist.py
@@ -15,6 +15,12 @@ import os
 import cftime
 import calendar
 
+# Plotting imports
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+from math import radians, cos, sin, asin, sqrt
+from math import atan2
+
 
 def roll_longitude(variable):
     '''
@@ -42,3 +48,30 @@ def roll_longitude(variable):
     lon_360 = lon_0.assign_coords(lon=360.)
     rolled_variable = xr.concat([variable, lon_360], dim='lon')
     return rolled_variable
+
+def set_fontsize(titles=24, labels=20, legend=20, other=16):
+    '''
+    Set matplotlib default fontsizes with plt.rc()
+
+    Parameters
+    ----------
+    titles: integer
+        font size applied to axes titles and figure titles
+        Default is 24
+    labels: integer
+        font size applied to x- and y-axis labels
+        Defult is 20
+    legend: integer
+        font size applied to legend
+        Defult is 20
+    other: integer
+        font size applied to x- and y-ticks and all other text
+        Default is 16
+    '''
+    plt.rc('font', size=other)           # controls default text sizes
+    plt.rc('axes', titlesize=titles)     # fontsize of the axes title
+    plt.rc('axes', labelsize=labels)     # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=other)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=other)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=legend)    # legend fontsize
+    plt.rc('figure', titlesize=titles)   # fontsize of the figure title
